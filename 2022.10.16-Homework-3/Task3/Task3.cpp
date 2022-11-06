8cd9f8d3b2bf53b2684d558495db7294dcf076b3
st105974 <i_yarkin@inbox.ru>
2022-11-06 23:17:46 +0300

    123

---
+++ 2022.10.16-Homework-3/Task3/Task3.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    int n = 0;
+    int k = 1;
+    int l = 1;
+    std::cin >> n;
+    do
+    {
+        do
+        {
+            std::cout << k++ << " ";
+            
+
+        } while (k != n + l);
+        std::cout << std::endl;
+        l++;
+        k = l;
+        
+        
+        
+    } while (k <= n);
+    return EXIT_SUCCESS
+ 
+
+    
+    
+}
+
+
